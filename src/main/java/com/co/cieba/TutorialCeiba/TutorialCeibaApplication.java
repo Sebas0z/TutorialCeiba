ef8097d5553eee35589b4d88ff74daa90810854a
CEIBA\sebastian.vasquez <sebastian.vasquez@ceiba.com.co>
2018-05-03 16:20:44 -0500

    Codigo Base

---
+++ src/main/java/com/co/cieba/TutorialCeiba/TutorialCeibaApplication.java
@@ -0,0 +1,12 @@
+package com.co.cieba.TutorialCeiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialCeibaApplication.class, args);
+	}
+}
